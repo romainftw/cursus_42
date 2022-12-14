0ff20d7c4d30aab28f37f0f09dbc6c0180de1942
Romain Perrin <roperrin@student.42nice>
2022-12-14 12:12:43 +0100

    allerady update ?

--- pushswap/push_swap.h
+++ pushswap/push_swap.h
@@ -6,12 +6,18 @@
 /*   By: roperrin <roperrin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 14:11:52 by roperrin          #+#    #+#             */
-/*   Updated: 2022/12/09 16:38:31 by roperrin         ###   ########.fr       */
+/*   Updated: 2022/12/09 20:59:26 by roperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+typedef struct listing
+{
+	int		element;
+	listing	*next;
+}t_listing;
 
 void	push_swap(char *tab);
 size_t	ft_strlen(const char *s);
