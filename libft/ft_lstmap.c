06b08ddec133a969e1b032d84cf3597306d956d5
Romain Perrin <roperrin@student.42nice>
2022-12-14 19:59:10 +0100

    FU*K whit <3

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: roperrin <roperrin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 17:26:02 by roperrin          #+#    #+#             */
-/*   Updated: 2022/12/14 18:51:47 by roperrin         ###   ########.fr       */
+/*   Updated: 2022/12/14 19:42:39 by roperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new;
 
 	temp = lst;
-	new = ft_lstnew(f(temp->content));
 	if (!lst || !f || !del)
 		return (NULL);
+	//new = ft_lstnew(f(temp->content));
+	new = NULL;
 	while (temp)
 	{
-		new->next = new->next;
-		new->content = ft_lstnew(f(temp->content));
+		new = ft_lstnew(f(temp->content));
 		temp = temp->next;
+		//new->next = temp;
+		new = new->next;
 	}
 	new->next = NULL;
 	return (new);
