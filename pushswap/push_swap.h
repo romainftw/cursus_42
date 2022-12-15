3cbc46a6afb6e234cb300f3bb93788e5725d3908
Romain Perrin <romain.nice@gmail.com>
2022-12-15 19:04:30 +0100

    test

--- pushswap/push_swap.h
+++ pushswap/push_swap.h
@@ -6,18 +6,18 @@
 /*   By: roperrin <roperrin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 14:11:52 by roperrin          #+#    #+#             */
-/*   Updated: 2022/12/09 20:59:26 by roperrin         ###   ########.fr       */
+/*   Updated: 2022/12/15 18:50:50 by roperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-typedef struct listing
+typedef struct s_list
 {
 	int		element;
-	listing	*next;
-}t_listing;
+	s_list	*next;
+}t_list;
 
 void	push_swap(char *tab);
 size_t	ft_strlen(const char *s);
