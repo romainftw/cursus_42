23c68c4e36102603ba896bbffdf364ceeea9f9b6
Romain Perrin <romain.nice@gmail.com>
2023-01-26 17:34:48 +0100

    algo ok

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,28 +6,52 @@
 /*   By: roperrin <roperrin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 15:06:59 by roperrin          #+#    #+#             */
-/*   Updated: 2023/01/26 12:27:55 by roperrin         ###   ########.fr       */
+/*   Updated: 2023/01/26 17:33:08 by roperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	receveid(char *str)
+void	receveid(char *str, int pid_n)
 {
-	ft_printf("%s", str);
+	int	bin;
+	int	k;
+	int	i;
+
+	k = 7;
+	i = 0;
+	bin = 0;
+	while (str[i])
+	{
+		while (k >= 0)
+		{
+			bin = (str[i] >> k & 1);
+			if (bin == 1)
+				kill(pid_n, SIGUSR1);
+			if (bin == 0)
+				kill(pid_n, SIGUSR2);
+			k--;
+			usleep(5000);
+		}
+		k = 7;
+		i++;
+	}
 }
 
 int	main(int arc, char **arg)
 {	
-	int	i;
+	int		i;
+	int		pid_n;
 
 	i = 1;
 	if (arc >= 2)
 	{
-		ft_printf("client start ta mere\n");
+		ft_printf("client start sa mere\n");
+		pid_n = ft_atoi(arg[i]);
+		i++;
 		while (arg[i])
 		{
-			receveid(arg[i]);
+			receveid(arg[i], pid_n);
 			i++;
 		}
 	}
--- minitalk/serveur.c
+++ minitalk/serveur.c
@@ -6,15 +6,58 @@
 /*   By: roperrin <roperrin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 15:05:45 by roperrin          #+#    #+#             */
-/*   Updated: 2023/01/26 12:31:03 by roperrin         ###   ########.fr       */
+/*   Updated: 2023/01/26 17:28:22 by roperrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+// void	signal_recever(int sig)
+// {
+// 	if (sig == SIGUSR1)
+// 		ft_printf("1\n");
+// 	else if (sig == SIGUSR2)
+// 		ft_printf("0\n");
+// 	signal(sig, signal_recever);
+// }
+
+void convertion(int sign)
+{
+	static char	x = 0;
+	static int	i = 0;
+	
+	if (sign == 30)
+		x = (x << 1 | 1);
+	if (sign == 31)
+		x = (x << 1 & ~0);
+	i++;
+	// ft_printf("%d ", sign);
+	// int	h = 7;
+	// ft_printf("Etat de x :");
+	// while (h >= 0)
+	// {
+	// 	ft_printf(" %d", ((x >> h) & 1));
+	// 	h--;
+	// }
+	// ft_printf("\n");
+	if (i == 8)
+	{
+		ft_printf("%c", x);
+		i = 0;
+		x = 0;
+	}
+}
+
 int	main(void)
 {
-	printf("vas bien niquer ta mere");
+	int	pid_n;
 
+	pid_n = getpid();
+	ft_printf("PID : %d\n", pid_n);
+	signal(SIGUSR1, convertion);
+	signal(SIGUSR2, convertion);
+	while (1)
+	{
+	}
 	return (0);
 }
